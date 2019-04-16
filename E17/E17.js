a1be4bf30ce739c112fdd0e7d2b93c26e39593c8
scarletheartz <46933900+scarletheartz@users.noreply.github.com>
2019-04-16 11:39:18 -0400

    Day 24

--- E17/E17.js
+++ E17/E17.js
@@ -20,30 +20,4 @@ $(document).ready(function() {
 
 function colorGuess() {
     var colorArray = ["Black","Green","LightBlue","LightGrey","NavyBlue","NeonYellow","Pink","Red","RobinBlue","SkyBlue","White","WineRed"];
-    var randomNumber = Math.floor(Math.random() * colorArray.length);
-    var stripped = colorArray[randomNumber].toLowerCase();
-    console.log(colorArray[randomNumber]);
-    console.log(stripped);
-    $('body').css('background-color', colorArray[randomNumber]);
-    $('#submit').click(function() {
-        var input = $('input').val();
-        var guess = input.toLowerCase();
-        console.log(guess);
-        if (guess == stripped || guess == colorArray[randomNumber]) {
-            $('h1').text(guess + ' is right!');
-            $('input').val('');
-            $('.button p').text('Play Again?');
-            $('#submit').click(function() {
-                location.reload(true);
-            });
-        } else  {
-            $('h1').text(guess + ' is wrong');
-            $('input').val('');
-            $('.button p').text('Play Again?');
-            $('#submit').click(function() {
-                location.reload(true);
-            });
-        }
-    })
-}
 })(jQuery);

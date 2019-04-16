181eb63aaf244d9ad5559c29ac0225da829605a0
scarletheartz <46933900+scarletheartz@users.noreply.github.com>
2019-04-16 11:25:43 -0400

    Day 24

--- E17/E17.js
+++ E17/E17.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 });
 
 function colorGuess() {
-    var colorArray = ["Black","Green","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RobinBlue","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","WineRed","Yellow","YellowGreen"];
+    var colorArray = ["Black","Green","LightBlue","LightGrey","NavyBlue","NeonYellow","Pink","Red","RobinBlue","SkyBlue","White","WineRed"];
     var randomNumber = Math.floor(Math.random() * colorArray.length);
     var stripped = colorArray[randomNumber].toLowerCase();
     console.log(colorArray[randomNumber]);

6f84adfbede4f87c8054ce0353870263acc1f190
scarletheartz <46933900+scarletheartz@users.noreply.github.com>
2019-04-18 11:44:33 -0400

    Day 25

--- E17/E17.js
+++ E17/E17.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
     // run function on initial page load
     colorGuess();
 });
-
-function colorGuess() {
+(function colorGuess() {
     //color variable
     var colorArray = ["Black","Green","LightBlue","LightGrey","NavyBlue","NeonYellow","Pink","Red","RobinBlue","SkyBlue","White","WineRed"];
+    //image link variable
+    var imageArray = (colorArray[random number variable name] + ".png");
     //random variable
     var randomNumber = Math.floor(Math.random() * colorArray.length);
-    //image link variable
-    var imageArray = colorArray[random number variable name] + ".png"
+    var stripped = colorArray[randomNumber].toLowerCase();
     console.log(colorArray[randomNumber]);
+    console.log(stripped);
     console.log(imageArray);
     $('body').css("", colorArray[randomNumber]);
     $("#submit").click(function() {
@@ -22,19 +23,19 @@ function colorGuess() {
         $("input").val("");
         $(".button p").text("Play Again?");
         $("#submit").click(function() {
-          location.reload(true);
+          imageArray.reload(true);
         });
-      } else {
+        }
+      });
+}
+      else {
         $("h1").text(guess + "Incorrect");
         $("input").val("");
         $(".button p").text("Play Again?");
         $("#submit").click(function() {
-          location.reload(true);
-        });
-      }
-    })
-}
-
+          imageArray.reload(true);
+        })
+      });
 (function($) {
   $(function() { // DOM Ready
 

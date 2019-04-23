7c41cc760fe55db71d804496dd6ec770952be604
scarletheartz <46933900+scarletheartz@users.noreply.github.com>
2019-04-23 12:14:07 -0400

    Day 26

--- P5/P5.js
+++ P5/P5.js
@@ -16,20 +16,43 @@
   });
 })(jQuery);
 
-(function($) {
-  $(function() { // DOM Ready
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
 
-    // Toggle navigation
-    $('#nav-toggle').click(function() {
-      this.classList.toggle("active");
-      // If sidebar is visible:
-      if ($('body').hasClass('show-nav')) {
-        // Hide sidebar
-        $('body').removeClass('show-nav');
-      } else { // If sidebar is hidden:
-        $('body').addClass('show-nav');
-        // Display sidebar
-      }
-    });
+// Declare a loop variable
+var i;
+
+// Show first 10 pokemon
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Show next 10 pokemon
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
-})(jQuery);
+}
